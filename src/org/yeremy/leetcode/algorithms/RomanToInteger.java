e4bcda966c362479a16065c2c2df28add5613399
Yeremy Turcios <yeremyturcios@gmail.com>
2018-02-15 06:45:14 -0700

    Improving solution for RomanToInteger

--- src/org/yeremy/leetcode/algorithms/RomanToInteger.java
+++ src/org/yeremy/leetcode/algorithms/RomanToInteger.java
@@ -1,37 +1,18 @@
 package org.yeremy.leetcode.algorithms;
+/*
+
+- Make a map of roman numerals to integers
+- Initialize sum to the last element of the string (in integer)
+- Iterate through string backwards starting i at s.length - 2
+- if s[i] >= sum, then add it to sum
+- else then substract it from sum
+*/
 
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-I = 1
-V = 5 CANNOT BE REPEATED
-X = 10
-L = 50 CANNOT BE REPEATED
-C = 100
-D = 500 CANNOT BE REPEATED
-M = 1000
-
-letters cannot be repeated more than 3 times, except for V, L, D
-
-Numbers that decrease from left to right are added
-A smaller number at the left of a larger number is a substraction
-
-MMMCMXCIX
-
-- Convert the string to char array
-- Create a map of roman letters to numbers
-- Read from left to right
-- Initialize variable sum to 0
-- Initialize curr and prev to 0
-- Intialize left to 0 and right to 1
-- If curr > prev; sum -= (curr + prev); sum += (curr - prev) // backtracks and then recalculates
-*/
 public class RomanToInteger {
-    public static void main(String[] args) {
-        System.out.println(romanToInt("MMMCMXCIX"));
-    }
-    public static int romanToInt(String s) {
+    public int romanToInt(String s) {
         char[] romans = s.toCharArray();
 
         Map<Character, Integer> romanMap = new HashMap<>();
@@ -44,21 +25,15 @@ public class RomanToInteger {
         romanMap.put('M', 1000);
 
         int length = romans.length;
-        int sum = 0;
-        int index = 0;
-        int curr = 0;
-        int prev = 0;
+        int sum = romanMap.get(romans[length - 1]);
 
-        while (index < length) {
-            curr = romanMap.get(romans[index++]);
-            sum += curr;
-
-            if (curr > prev && prev != 0) {
-                sum -= (curr + prev);
-                sum += (curr - prev);
-            }
-
-            prev = curr;
+        for (int i = romans.length - 2; i >= 0; i--) {
+            int left = romanMap.get(romans[i]);
+            int right = romanMap.get(romans[i + 1]);
+            if ( left >= right )
+                sum += left;
+            else
+                sum -= left;
         }
 
         return sum;
