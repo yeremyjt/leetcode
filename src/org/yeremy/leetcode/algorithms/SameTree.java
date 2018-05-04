f194cc999437edd440ad6d9a2faee7d6bbdb3fe4
Yeremy Turcios <yeremyturcios@gmail.com>
2018-05-03 22:31:55 -0600

    Cleaning solution for SameTree.

--- src/org/yeremy/leetcode/algorithms/SameTree.java
+++ src/org/yeremy/leetcode/algorithms/SameTree.java
@@ -17,23 +17,9 @@ public class SameTree
 
     public static boolean isSameTree(TreeNode p, TreeNode q)
     {
-        if ((p == null && q != null) || (p != null && q == null))
-            return false;
-        if (p != null && q != null)
-        {
-            if (p.val != q.val)
-            {
-                return false;
-            }
-        }
-        if (p == null && q == null)
-        {
-            return true;
-        }
-        if (isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
-            return true;
-        else
-            return false;
+        if (p == null && q == null) return true;
+        if ((p == null && q != null) || (p != null && q == null) || p.val != q.val) return false;
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
     }
 
     private static class TreeNode
