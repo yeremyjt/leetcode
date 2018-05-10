a4a4645deb231329158780b6e2d6a1b9a74bfa92
Yeremy Turcios <yeremyturcios@gmail.com>
2018-05-09 23:39:12 -0600

    Added a better example for Minimum Depth of Binary Tree.

--- src/org/yeremy/leetcode/algorithms/MinimumDepthOfBinaryTree.java
+++ src/org/yeremy/leetcode/algorithms/MinimumDepthOfBinaryTree.java
@@ -4,8 +4,11 @@ public class MinimumDepthOfBinaryTree {
 
     public static void main(String[] args)
     {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
         System.out.println(minDepth(root));
     }
 
