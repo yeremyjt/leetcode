2fc6da175489752e20e01f8ee6f9a40823cf59e3
Yeremy Turcios <yeremyturcios@gmail.com>
2018-06-06 22:42:43 -0600

    Fixed bug.

--- src/org/yeremy/leetcode/algorithms/KnightProbabilityInChessboard.java
+++ src/org/yeremy/leetcode/algorithms/KnightProbabilityInChessboard.java
@@ -7,21 +7,21 @@ public class KnightProbabilityInChessboard
     private int[][] moves = {{1, 2}, {1, -2}, {2, 1}, {2, -1}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}};
 
     public double knightProbability(int N, int K, int r, int c) {
-        int[][] validCount = new int[N][N];
-        for (int[] row : validCount) {
-             Arrays.fill(row, 1);
+        double[][] validCount = new double[N][N];
+        for (double[] row : validCount) {
+            Arrays.fill(row, 1);
         }
 
-        for(int k = 0; k < K; k++) { // Iterate through all possible moves
-            int[][] currentValidCount = new int[N][N];
+        for(int k = 0; k < K; k++) { // Iterate through K moves
+            double[][] currentValidCount = new double[N][N];
             for (int i = 0; i < N; i++) {  // Filling out the board with count of valid moves per cell
                 for (int j = 0; j < N; j++) {
-                     for (int[] move: moves) {
-                         int row = i + move[0];
-                         int col = j + move[1];
-                         if (isValid(row, col, N))
-                             currentValidCount[i][j] += validCount[row][col];
-                     }
+                    for (int[] move: moves) { // Going through every possible move
+                        int row = i + move[0];
+                        int col = j + move[1];
+                        if (isValid(row, col, N))
+                            currentValidCount[i][j] += validCount[row][col];
+                    }
                 }
             }
             validCount = currentValidCount;
@@ -33,4 +33,5 @@ public class KnightProbabilityInChessboard
     private boolean isValid(int row, int col, int N) {
         return (row >= 0 && row < N && col >= 0 && col < N);
     }
+}
 }
