a4e13f20738b7cfa60d4eaf7ba2e872cf4d7d101
Yeremy Turcios <yeremyturcios@gmail.com>
2018-06-14 20:54:00 -0600

    Writing optimal solution for Reverse Linked List.

--- src/org/yeremy/leetcode/algorithms/ReverseLinkedList.java
+++ src/org/yeremy/leetcode/algorithms/ReverseLinkedList.java
@@ -1,31 +1,21 @@
 package org.yeremy.leetcode.algorithms;
 
-import java.util.Stack;
-
 public class ReverseLinkedList
 {
     public ListNode reverseListIterative(ListNode head) {
         if (head == null) return null;
 
-        Stack<ListNode> stack = new Stack();
-        ListNode node = head;
-        stack.push(node);
+        ListNode prev = null;
+        ListNode curr = head;
 
-        while (node.next != null) {
-            stack.push(node.next);
-            node = node.next;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
         }
 
-        head = node;
-
-        while(!stack.empty()) {
-            node.next = stack.pop();
-            node = node.next;
-        }
-
-        node.next = null;
-
-        return head;
+        return prev;
     }
 
     public ListNode reverseListRecursively(ListNode head) {
