c098e27f039b56eebe663cc567c88dfb982b8389
Yeremy Turcios <yeremyturcios@gmail.com>
2018-08-29 17:01:26 -0600

    Solved Lowest Common Ancestor in Binary Search Tree.

---
+++ src/org/yeremy/leetcode/algorithms/binarytree/LowestCommonAncestorBinarySearchTree.java
@@ -0,0 +1,15 @@
+package org.yeremy.leetcode.algorithms.binarytree;
+
+import org.yeremy.leetcode.algorithms.TreeNode;
+
+public class LowestCommonAncestorBinarySearchTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return root;
+    }
+}
---
+++ test/org/yeremy/leetcode/algorithms/binarytree/LowestCommonAncestorBinarySearchTreeTest.java
@@ -0,0 +1,25 @@
+package org.yeremy.leetcode.algorithms.binarytree;
+
+import org.junit.Test;
+import org.yeremy.leetcode.algorithms.TreeNode;
+
+import static org.junit.Assert.*;
+
+public class LowestCommonAncestorBinarySearchTreeTest {
+    @Test
+    public void testLowestCommonAncestor() {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+
+        LowestCommonAncestorBinarySearchTree lowestCommonAncestorBinarySearchTree = new LowestCommonAncestorBinarySearchTree();
+        TreeNode lowest = lowestCommonAncestorBinarySearchTree.lowestCommonAncestor(root, new TreeNode(2), new TreeNode(4));
+        assertEquals(2, lowest.val);
+    }
+}
