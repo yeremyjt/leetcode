fad620a81507186ddd63284cc5c58a73d9dfb3d4
Yeremy Turcios <yeremyturcios@gmail.com>
2018-10-28 14:09:26 -0600

    Solving binary tree level order traversal II

---
+++ src/org/yeremy/leetcode/algorithms/binarytree/BinaryTreeLevelOrderTraversalTwo.java
@@ -0,0 +1,29 @@
+package org.yeremy.leetcode.algorithms.binarytree;
+
+import org.yeremy.leetcode.algorithms.TreeNode;
+import java.util.*;
+
+public class BinaryTreeLevelOrderTraversalTwo {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new LinkedList<>();
+        queue.add(root);
+        if (root == null) return result;
+
+        while (!queue.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                if (queue.peek().left != null) queue.add(queue.peek().left);
+                if (queue.peek().right != null) queue.add(queue.peek().right);
+                levelList.add(queue.poll().val);
+            }
+
+            // Inserts the specified element at the specified position in this list. Shifts the element currently at
+            // that position (if any) and any subsequent elements to the right (adds one to their indices).
+            result.add(0, levelList);
+        }
+
+        return result;
+    }
+}
