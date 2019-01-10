9b32b864ed64b88f0dd51566b51fc8de71f27f24
Yeremy Turcios <yeremyturcios@gmail.com>
2019-01-10 07:59:26 -0700

    Adding more comments.

--- src/org/yeremy/leetcode/algorithms/stack/LongestAbsoluteFilePath.java
+++ src/org/yeremy/leetcode/algorithms/stack/LongestAbsoluteFilePath.java
@@ -9,12 +9,12 @@ public class LongestAbsoluteFilePath {
         stack.push(0); // "dummy" length
         int maxLen = 0;
         for(String s : input.split("\n")){
-            int lev = s.lastIndexOf("\t") + 1; // number of "\t"
+            int lev = s.lastIndexOf("\t") + 1; // number of "\t"'s
             while (lev + 1 < stack.size()) stack.pop(); // find parent
-            int len = stack.peek() + s.length() - lev + 1; // remove "/t", add"/"
+            int len = stack.peek() + s.length() - lev + 1; // remove all "/t"'s, add"/" at the end
             stack.push(len);
             // check if it is file
-            if(s.contains(".")) maxLen = Math.max(maxLen, len-1);
+            if(s.contains(".")) maxLen = Math.max(maxLen, len - 1); // subtracting the last "/" at the end
         }
         return maxLen;
     }
