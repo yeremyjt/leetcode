554ba7848212b591ad71f0dedb0cf50f9f22c2a0
Yeremy Turcios <yeremy@pop-os.localdomain>
2019-02-13 09:45:09 -0700

    Solved Repeated String Match

---
+++ src/org/yeremy/leetcode/algorithms/string/RepeatedStringMatch.java
@@ -0,0 +1,24 @@
+package org.yeremy.leetcode.algorithms.string;
+/*
+Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. If no such solution, return -1.
+
+For example, with A = "abcd" and B = "cdabcdab".
+
+Return 3, because by repeating A three times (“abcdabcdabcd”), B is a substring of it; and B is not a substring of A repeated two times ("abcdabcd").
+
+Note:
+The length of A and B will be between 1 and 10000.
+ */
+public class RepeatedStringMatch {
+    public int repeatedStringMatch(String A, String B) {
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
+        while(sb.length() < B.length()) {
+            sb.append(A);
+            counter++;
+        }
+        if (sb.toString().contains(B)) return counter;
+        if (sb.append(A).toString().contains(B)) return counter + 1;
+        return -1;
+    }
+}
---
+++ test/org/yeremy/leetcode/algorithms/string/RepeatedStringMatchTest.java
@@ -0,0 +1,16 @@
+package org.yeremy.leetcode.algorithms.string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RepeatedStringMatchTest {
+    @Test
+    public void testRepeatedStringMatch() {
+        String A = "abcd";
+        String B = "cdabcdab";
+        RepeatedStringMatch repeatedStringMatch  = new RepeatedStringMatch();
+        int result = repeatedStringMatch.repeatedStringMatch(A, B);
+        assertEquals(3, result);
+    }
+}
