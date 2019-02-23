185e3645892c9945f633dd0ab01c1fc2c61bd9ee
Yeremy Turcios <yturcios@ancestry.com>
2019-02-22 20:33:11 -0700

    Added BFS solution to Number of Islands

--- src/org/yeremy/leetcode/algorithms/graph/NumberOfIslands.java
+++ src/org/yeremy/leetcode/algorithms/graph/NumberOfIslands.java
@@ -1,5 +1,8 @@
 package org.yeremy.leetcode.algorithms.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NumberOfIslands {
     public int numIslands(char[][] grid) {
         if (grid.length == 0 || grid == null) return 0;
@@ -10,7 +13,7 @@ public class NumberOfIslands {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(i, j, grid);
+                    bfs(i, j, grid);
                     islandCounter++;
                 }
             }
@@ -31,5 +34,34 @@ public class NumberOfIslands {
         dfs(i + 1, j, grid); // down
         dfs(i, j + 1, grid); // right
         dfs(i, j - 1, grid); // left
+    }
+
+    private void bfs(int i , int j, char[][] grid) {
+        int rows = grid.length;
+        int columns = grid[0].length;
+        Cell root = new Cell(i, j);
+        Queue<Cell> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            Cell curr = queue.poll();
+            if (curr.i < 0 || curr.j < 0 || curr.i >= rows || curr.j >= columns || grid[curr.i][curr.j] == '0') continue;
+
+            grid[curr.i][curr.j] = '0';
+            queue.add(new Cell(curr.i - 1, curr.j)); // up
+            queue.add(new Cell(curr.i + 1, curr.j)); // down
+            queue.add(new Cell(curr.i, curr.j + 1)); // right
+            queue.add(new Cell(curr.i, curr.j - 1)); // left
+        }
+    }
+
+    private class Cell {
+        public int i;
+        public int j;
+
+        public Cell(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
     }
 }
