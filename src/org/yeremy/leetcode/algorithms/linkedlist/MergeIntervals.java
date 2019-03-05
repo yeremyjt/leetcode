41ea13d71a19c489b5d6aa8244aa14a05db92a6b
Yeremy Turcios <yturcios@ancestry.com>
2019-03-05 06:43:49 -0700

    Updated description of MergeIntervals

--- src/org/yeremy/leetcode/algorithms/linkedlist/MergeIntervals.java
+++ src/org/yeremy/leetcode/algorithms/linkedlist/MergeIntervals.java
@@ -8,9 +8,17 @@ import java.util.List;
 /*
 Given a collection of intervals, merge all overlapping intervals.
 
-For example,
-Given [1,3],[2,6],[8,10],[15,18],
-return [1,6],[8,10],[15,18].
+Example 1:
+
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Example 2:
+
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
  */
 public class MergeIntervals {
     public List<Interval> merge(List<Interval> intervals) {
