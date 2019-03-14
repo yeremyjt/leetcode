5b14923a6233a12f6180ee2ee342c0c051846c30
Yeremy Turcios <yturcios@ancestry.com>
2019-03-13 23:09:53 -0600

    Solved Maximum Subarray

---
+++ src/org/yeremy/leetcode/algorithms/dynamicprogramming/MaximumSubarray.java
@@ -0,0 +1,28 @@
+package org.yeremy.leetcode.algorithms.dynamicprogramming;
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ */
+public class MaximumSubarray {
+    public int maxSubArray(int[] A) {
+        int n = A.length;
+        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = A[0];
+        int max = dp[0];
+
+        for(int i = 1; i < n; i++){
+            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
---
+++ test/org/yeremy/leetcode/algorithms/dynamicprogramming/MaximumSubarrayTest.java
@@ -0,0 +1,36 @@
+package org.yeremy.leetcode.algorithms.dynamicprogramming;
+
+import org.junit.Test;
+import org.yeremy.leetcode.algorithms.dynamicprogramming.MaximumSubarray;
+
+import static org.junit.Assert.*;
+
+public class MaximumSubarrayTest {
+    @Test
+    public void testMaximumSubarray() {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        int actual = maximumSubarray.maxSubArray(nums);
+        int expected = 6;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMaximumSubarray_1() {
+        int[] nums = {1};
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        int actual = maximumSubarray.maxSubArray(nums);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMaximumSubarray_2() {
+        int[] nums = {8,-19,5,-4,20};
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+        int actual = maximumSubarray.maxSubArray(nums);
+        int expected = 21;
+        assertEquals(expected, actual);
+    }
+
+}
