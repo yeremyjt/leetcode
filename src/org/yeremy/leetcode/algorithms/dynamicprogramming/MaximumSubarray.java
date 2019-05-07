872ad2b9a5c16c0689be17820d3391cfbc8b8ced
Yeremy Turcios <yturcios@ancestry.com>
2019-05-06 23:16:55 -0600

    Updating Maximum Subarray

--- src/org/yeremy/leetcode/algorithms/dynamicprogramming/MaximumSubarray.java
+++ src/org/yeremy/leetcode/algorithms/dynamicprogramming/MaximumSubarray.java
@@ -13,16 +13,11 @@ If you have figured out the O(n) solution, try coding another solution using the
  */
 public class MaximumSubarray {
     public int maxSubArray(int[] A) {
-        int n = A.length;
-        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
-        dp[0] = A[0];
-        int max = dp[0];
-
-        for(int i = 1; i < n; i++){
-            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            max = Math.max(max, dp[i]);
+        int maxSoFar = A[0], maxEndingHere = A[0];
+        for (int i = 1; i < A.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere + A[i], A[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
         }
-
-        return max;
+        return maxSoFar;
     }
 }
