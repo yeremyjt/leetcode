5669065c10f8926ed88f340bda33ea889a8cf436
Yeremy Turcios <yeremy@pop-os.localdomain>
2019-05-27 15:28:42 -0600

    Solved Letter Combinations of a Phone Number

---
+++ src/org/yeremy/leetcode/algorithms/backtracking/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,39 @@
+package org.yeremy.leetcode.algorithms.backtracking;
+
+import java.util.*;
+
+public class LetterCombinationsOfAPhoneNumber {
+    Map<String, String> phone = new HashMap<String, String>() {{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+
+    List<String> combinations = new ArrayList<>();
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() > 0) {
+            backtrack("", digits);
+        }
+        return combinations;
+    }
+
+    private void backtrack(String combination, String digits) {
+        if (digits.length() == 0) {
+            combinations.add(combination);
+        }
+        else {
+            String digit = Character.toString(digits.charAt(0));
+            String letters = phone.get(digit);
+            for (int i = 0; i < letters.length(); i++) {
+                String letter = Character.toString(phone.get(digit).charAt(i));
+                backtrack(combination + letter, digits.substring(1));
+            }
+        }
+    }
+}
---
+++ test/org/yeremy/leetcode/algorithms/backtracking/LetterCombinationsOfAPhoneNumberTest.java
@@ -0,0 +1,27 @@
+package org.yeremy.leetcode.algorithms.backtracking;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LetterCombinationsOfAPhoneNumberTest {
+
+    @Test
+    public void letterCombinations() {
+        String digits = "23";
+        LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumber();
+        List<String> combinations = letterCombinationsOfAPhoneNumber.letterCombinations(digits);
+        assertEquals(9, combinations.size());
+        assertTrue(combinations.contains("ad"));
+        assertTrue(combinations.contains("ae"));
+        assertTrue(combinations.contains("af"));
+        assertTrue(combinations.contains("bd"));
+        assertTrue(combinations.contains("be"));
+        assertTrue(combinations.contains("bf"));
+        assertTrue(combinations.contains("cd"));
+        assertTrue(combinations.contains("ce"));
+        assertTrue(combinations.contains("cf"));
+    }
+}
