98ad97ea87bca6c5947aa2527870b583402741af
= <=>
2019-06-08 18:21:30 -0600

    Refactoring Trapping Rain Water

--- src/org/yeremy/leetcode/algorithms/dynamicprogramming/TrappingRainWater.java
+++ src/org/yeremy/leetcode/algorithms/dynamicprogramming/TrappingRainWater.java
@@ -2,27 +2,25 @@ package org.yeremy.leetcode.algorithms.dynamicprogramming;
 
 public class TrappingRainWater {
     public int trap(int[] height) {
-        if (height == null || height.length == 0) return 0;
-        int[] maxLeftArray = new int[height.length];
-        int[] maxRightArray = new int[height.length];
-        int maxLeft = 0;
-        int maxRight = 0;
-        int rainWater = 0;
+        int n = height.length;
+        int[] maxLeftArray = new int[n];
+        int[] maxRightArray = new int[n];
+        int maxLeft = 0, maxRight = 0, rainWater = 0;
 
         // Filling maxLeftArray
-        for (int i = 0; i < height.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (height[i] > maxLeft) maxLeft = height[i];
             maxLeftArray[i] = maxLeft;
         }
 
         // Filling maxRightArray
-        for (int i = height.length - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             if (height[i] > maxRight) maxRight = height[i];
             maxRightArray[i] = maxRight;
         }
 
         // Calculating rainWater
-        for (int i = 0; i < height.length; i++) {
+        for (int i = 0; i < n; i++) {
             rainWater += (Math.min(maxLeftArray[i], maxRightArray[i]) - height[i]);
         }
 
