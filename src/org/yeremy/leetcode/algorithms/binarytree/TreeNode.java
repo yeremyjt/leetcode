1af87a1bfde0c97fc3e4f94880fe9c6731aa428f
Yeremy Turcios <yeremyturcios@gmail.com>
2022-12-30 00:26:31 -0800

    Solve Find Duplicate Subtrees.

---
+++ src/org/yeremy/leetcode/algorithms/binarytree/TreeNode.java
@@ -0,0 +1,22 @@
+package org.yeremy.leetcode.algorithms.binarytree;
+
+/**
+ * Definition of a binary tree node.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    TreeNode() {}
+    
+    TreeNode(int val) { 
+        this.val = val; 
+    }
+    
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ test/org/yeremy/leetcode/algorithms/binarytree/FindDuplicateSubtreesTest.java
@@ -0,0 +1,28 @@
+package org.yeremy.leetcode.algorithms.binarytree;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FindDuplicateSubtreesTest {
+    
+    @Test
+    public void testFindDuplicateSubtrees() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(2);
+        root.right.left.left = new TreeNode(4);
+        root.right.right = new TreeNode(4);
+
+        FindDuplicateSubtrees findDuplicateSubtrees = new FindDuplicateSubtrees();
+        List<TreeNode> actual = findDuplicateSubtrees.findDuplicateSubtrees(root);
+
+        List<TreeNode> expected = Arrays.asList(root.left.left, root.left);
+        assertTrue(expected.containsAll(actual));
+    }
+}
