82d663655ad9ec54bab58a20845e37b4da606983
Yeremy Turcios <yeremyturcios@gmail.com>
2023-01-02 12:04:04 -0800

    Solved Jewels In Stones.

--- src/org/yeremy/leetcode/algorithms/hashtable/JewelsAndStones.java
+++ src/org/yeremy/leetcode/algorithms/hashtable/JewelsAndStones.java
@@ -1,12 +1,11 @@
 package org.yeremy.leetcode.algorithms.hashtable;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /*
-You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have.
+Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
@@ -33,26 +32,24 @@ All the characters of jewels are unique.
 
     /* Algorithm:
      * Iterate over stone string and get unique characters and put them in a map. 
-     * Iterate over jewels and increement count for each character in map found in the string.
+     * Iterate over jewels and increment count for each character in map found in the string.
      * Sum all the values in map and return sum. 
      */
-    private Set<Character> stoneSet = new HashSet<>();
+    private final Set<Character> jewelsSet = new HashSet<>();
     
     public int numJewelsInStones(String jewels, String stones) {
         int count = 0;
-        // Creating map. 
-        char[] stonesArray = stones.toCharArray();
-        
-        for (int i = 0; i < stonesArray.length; i++) {
-            if (!stoneSet.contains(stonesArray[i])) {
-                stoneSet.add(stonesArray[i]);
-            }
-        }
-        
+        // Creating map.
         char[] jewelsArray = jewels.toCharArray();
-        
-        for (int i = 0; i < jewelsArray.length; i++) {
-            if (stoneSet.contains(jewelsArray[i])) {
+
+        for (char c : jewelsArray) {
+            jewelsSet.add(c);
+        }
+
+        char[] stonesArray = stones.toCharArray();
+
+        for (char c : stonesArray) {
+            if (jewelsSet.contains(c)) {
                 count++;
             }
         }
