ad8e247715c0d576e323ddd72ecc6ce8f8208636
Yeremy Turcios <yeremyturcios@gmail.com>
2023-01-21 15:51:32 -0800

    Solved Number of Provinces.

---
+++ test/org/yeremy/leetcode/algorithms/graph/NumberOfProvincesTest.java
@@ -0,0 +1,26 @@
+package org.yeremy.leetcode.algorithms.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumberOfProvincesTest {
+
+    @Test
+    public void findCircleNum() {
+        int[][] isConnected = new int[3][3];
+        isConnected[0][0] = 1;
+        isConnected[0][1] = 1;
+        isConnected[0][2] = 0;
+        isConnected[1][0] = 1;
+        isConnected[1][1] = 1;
+        isConnected[1][2] = 0;
+        isConnected[2][0] = 0;
+        isConnected[2][1] = 0;
+        isConnected[2][2] = 1;
+        NumberOfProvinces numberOfProvinces = new NumberOfProvinces();
+        int actual = numberOfProvinces.findCircleNum(isConnected);
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+}
